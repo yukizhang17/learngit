918c8849422c57511700fed7cc1d8572e6560a73
Yuqi Zhang <yuqizhang@dyn-160-39-8-32.dyn.columbia.edu>
2021-09-11 19:25:10 -0400

    Create a test.py

---
+++ test.py
@@ -0,0 +1,8 @@
+def maxSubArray(nums):
+	current_subarray = max_subarray = nums[0]
+	for num in nums[1:]:
+		current_subarray = max(num, current_subarray + num)
+		max_subarray = max(max_subarray, current_subarray)
+	return max_subarray
+
+print(maxSubArray([1,2,3-5,4,2,4]))
